36196ca3faedd3003c9a366f5ce2dbf206efb273
OnoprienkoArtem <o.artem.e@gmail.com>
2019-05-01 12:30:00 +0300

    add functionality for delete item

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Header msg="Welcome to Your Vue.js App"/>
-    <Todos v-bind:todos='todos'/>
+    <Todos v-bind:todos='todos' v-on:del-todo="deleteTodo"/>
 
   </div>
 </template>
@@ -36,6 +36,11 @@ export default {
           completed: false 
         }
       ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
     }
   }
 };
