c6aa84195357cd8c29d0b208e3071cd984bb392e
OnoprienkoArtem <o.artem.e@gmail.com>
2019-05-02 12:02:58 +0300

    add store modules

--- vue-vuex-todo/src/App.vue
+++ vue-vuex-todo/src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <div id="app">
-    <Todos />
+    <div class="container">
+      <Todos/>
+    </div>
   </div>
 </template>
 
 <script>
-  import Todos from "./components/Todos.vue";
+import Todos from "./components/Todos.vue";
 
-  export default {
-    name: "app",
-    components: {
-      Todos
-    }
-  };
+export default {
+  name: "app",
+  components: {
+    Todos
+  }
+};
 </script>
 
 <style>
-  body {
-    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-    line-height: 1.6;
-    background: #e8f7f0;
-  }
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
 
-  .container {
-    max-width: 1100px;
-    margin: auto;
-    overflow: auto;
-    padding: 0 2rem;
-  }
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
 </style>
